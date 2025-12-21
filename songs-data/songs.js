b151fe92f0314e49f84e41762136478375f76d73
vsvptech <111567939+vsvptech@users.noreply.github.com>
2025-12-21 07:40:41 +0530

    Update songs.js

--- songs-data/songs.js
+++ songs-data/songs.js
@@ -181,10 +181,10 @@ const songs = [
         artist: "Mappillai",
         image: getFirebaseUrl("images/Mappillai.jpg"),
         audio: {
-            male: getFirebaseUrl("songs/male/Enna-Thaan-Sugano-Male.mp3"),
-            female: getFirebaseUrl("songs/female/Enna-Thaan-Sugano-Female.mp3"),
-            duet: getFirebaseUrl("songs/duet/Enna-Thaan-Sugano-Duet.mp3"),
-            song: getFirebaseUrl("songs/song/Enna-Thaan-Sugano-Song.mp3")
+            male: getFirebaseUrl("songs/male/Enna-Thaan-Sugamo-Male.mp3"),
+            female: getFirebaseUrl("songs/female/Enna-Thaan-Sugamo-Female.mp3"),
+            duet: getFirebaseUrl("songs/duet/Enna-Thaan-Sugamo-Duet.mp3"),
+            song: getFirebaseUrl("songs/song/Enna-Thaan-Sugamo-Song.mp3")
         },  
         lyrics: "lyrics/EnnaThanSugamo.txt",
         availableTypes: ["male", "female", "duet", "song", "podcast"],
